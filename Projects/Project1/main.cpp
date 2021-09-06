a1dacdaab75dd3b5878687dc434e04d4d39e7f1e
Essspressso <hologram1220@gmail.com>
2021-09-07 00:30:31 +0900

    feat: add projects folder

---
+++ Projects/Project1/main.cpp
@@ -0,0 +1,19 @@
+// #include <QApplication>
+// #include <QPushButton>
+
+// int main(int argc, char **argv)
+// {
+//  QApplication app (argc, argv);
+
+//  QPushButton button ("Hello world !");
+//  button.show();
+
+//  return app.exec();
+// }
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    std::cout << "Hello world" << std::endl;
+}
